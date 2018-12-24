0fa23f3a262db88d69391d5dea39cc0eae808b16
steamme <nikita258963@gmail.com>
2018-12-24 21:37:27 +0200

    222

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ bot = telebot.TeleBot('749857527:AAGMZgPom3lE7t_wHcxDC9YmTgRju_6Ll40')
 def handlle_text (message):
          bot.send_message(message.from_user.id, 'Opening')
          r = requests.get("https://google.com")
-         print r.status_code
+         print(r.status_code)
 
 @bot.message_handler(commands=['link'])
 def handlle_text(message):
